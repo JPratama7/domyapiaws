492564e4544d873df73557f315c01e8385d9fc08
JPratama7 <josepratama080@gmail.com>
2024-07-24 12:40:19 +0700

    ganti ke proxywithcontext

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ import (
 func main() {
 	http.HandleFunc("/", route.URL)
 	adapter := httpadapter.New(http.DefaultServeMux)
-	lambda.Start(adapter.Proxy)
+	lambda.Start(adapter.ProxyWithContext)
 }
